a924f864808214b3ba76fd3ecce8abfe287baab4
mryangzhanpeng <2681107589@qq.com>
2019-12-17 13:14:35 +0800

    第二次提交

--- src/main/java/com/springcloud/springcloudeurekauser/controller/UserController.java
+++ src/main/java/com/springcloud/springcloudeurekauser/controller/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    /**
+     *
+     */
     @Autowired
     private UserService userService;
 
