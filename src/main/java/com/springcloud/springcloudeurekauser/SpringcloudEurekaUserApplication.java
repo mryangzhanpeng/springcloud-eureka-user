664ea5143c166a00329bd4055421dc661c5aa4af
mryangzhanpeng <2681107589@qq.com>
2019-12-17 13:37:31 +0800

    第三 次提交

--- src/main/java/com/springcloud/springcloudeurekauser/SpringcloudEurekaUserApplication.java
+++ src/main/java/com/springcloud/springcloudeurekauser/SpringcloudEurekaUserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
- * 启动类
+ * 启动类加
  */
 SpringBootApplication
 @EnableEurekaClient
